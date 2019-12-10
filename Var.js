0b2af3e006718dd975a43c4e5e11c8c7adb69a24
Rance Timi <rancecreation@gmail.com>
2019-12-10 18:31:22 +0100

    first commit

---
+++ Var.js
@@ -0,0 +1,13 @@
+var one = 20;
+var two = 19;
+var greet = "Variables"
+var add = one + two;
+var minus = one - two;
+var multiply = one * two;
+var divide = one/two;
+document.write(greet)
+document.write("First No: = " + one + "<br/>Second No: = " + two + " <br />");
+document.write(one + " + " + two + " = " + add + "<br/>");
+document.write(one + " - " + two + " = " + minus + "<br/>");
+document.write(one + " * " + two + " = " + multiply + "<br/>");
+document.write(one + " / " + two + " = " + divide + "<br/>");
